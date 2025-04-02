23581c181cae7b2b8a913ac97f2c153233f712b1
AKgalv1x <akgalvis@gmail.com>
2025-04-02 08:32:31 -0500

    ejercicio de for

---
+++ For_natha.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package for_natha;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class For_natha {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String chocolates[] = {"leche", "amargo", "blanco", "con mani"};
+        
+        for (int i=2; i < chocolates.length; i++){
+            System.out.println(chocolates[i]);
+        }
+    }
+    
+}
